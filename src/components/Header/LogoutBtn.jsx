6fe34cbed84848bb69271a1763c50daa822303f3
Jujhar Singh Sandhu <sjujhar532@gmail.com>
2025-01-27 21:33:23 +0530

    Add navigation to login page after logout

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import authService from '../../appwrite/auth'
 import { logout } from '../../store/authSlice'
+import { useNavigate } from 'react-router-dom'
 
 function LogoutBtn() {
 
     const dispatch = useDispatch()
+    const navigate = useNavigate()
     const logoutHandler = () =>{
         authService.logout().then(()=>{
             dispatch(logout())
+            navigate('/login')
         })
     }
 
